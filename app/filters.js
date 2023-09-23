029dd0dd0d69c947bf45c6a8248a574e272db4c1
Adam Silver <adambsilver@gmail.com>
2023-09-23 07:59:21 +0100

    Use x-gov filters

--- app/filters.js
+++ app/filters.js
@@ -1,25 +1,2 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
-
-const { marked } = require('marked');
-const GovukHTMLRenderer = require('govuk-markdown')
-
-addFilter('govukMarkdown', function(string, kwargs) {
-  if(typeof string !== 'string') {
-    return
-  }
-
-  const options = {
-    headingsStartWith: 'xl',
-    ...kwargs
-  }
-
-  marked.setOptions({
-    ...options,
-    headerIds: false, // Quieten deprecation warning in marked.js 5.x
-    mangle: false, // Quieten deprecation warning in marked.js 5.x
-    renderer: new GovukHTMLRenderer()
-  })
-
-  return marked(string)
-}, { renderAsHtml: true })
